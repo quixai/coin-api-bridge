16dd2b88c17f7c256656468a22244ca2fd8b4a64
tomas-quix <78492422+tomas-quix@users.noreply.github.com>
2021-04-22 17:26:30 +0200

    Update main.py (#2)

--- main.py
+++ main.py
@@ -1,43 +1,48 @@
-from datetime import datetime
-import pandas as pd
-
+import requests
 from quixstreaming import *
+import time
+from dateutil import parser
+import os
+import traceback
 
 # Create a client factory. Factory helps you create StreamingClient (see below) a little bit easier
-security = SecurityOptions('../certificates/ca.cert', "quixdev-textprocessingbench", "0XfH3dwvcIoYHLIpI2o4gm28jfPWJlCmiBy")
-client = StreamingClient('kafka-k1.quix.ai:9093,kafka-k2.quix.ai:9093,kafka-k3.quix.ai:9093', security)
+security = SecurityOptions('{placeholder:broker.security.certificatepath}', "{placeholder:broker.security.username}", "{placeholder:broker.security.password}")
+client = StreamingClient('{placeholder:broker.address}', security)
 
-# Open output topic connection.
-output_topic = client.open_output_topic('quixdev-textprocessingbench-lorem')
+output_topic = client.open_output_topic('{placeholder:topic}')
 
-# Create a new stream. A stream is a collection of data that belong to a single session of a single source.
-# For example single car journey.
-# If you don't specify stream id, random guid is generated.
-# Specify it if you want append data into the stream later.
-# stream = output_topic.create_stream("my-own-stream-id")
-stream = output_topic.create_stream()
+
+from_currency = "BTC"
+to_currency = "USD,GBP"
+
+url = 'https://rest.coinapi.io/v1/exchangerate/{0}?filter_asset_id={1}'.format(from_currency, to_currency)
+headers = {'X-CoinAPI-Key': '{placeholder:token}'}
+
+stream = output_topic.create_stream("coin-api")
 
 # Give the stream human readable name. This name will appear in data catalogue.
-stream.properties.name = "cardata"
+stream.properties.name = "Coin API"
 
 # Save stream in specific folder in data catalogue to help organize your workspace.
-stream.properties.location = "/static data"
+stream.properties.location = "/Coin API"
 
-# Add stream metadata to add context to time series data.
-stream.properties.metadata["circuit"] = "Sakhir Short"
-stream.properties.metadata["player"] = "Swal"
-stream.properties.metadata["game"] = "Codemasters F1 2019"
+while True:
+    try:
+        response = requests.get(url, headers=headers)
 
-df = pd.read_csv("cardata.csv")
+        data = response.json()
 
-# Add TAG__ prefix to column LapNumber to use this column as tag (index).
-df = df.rename(columns={"LapNumber" : "TAG__LapNumber" })
+        rows = data['rates']
 
-# Write data frame to output topic.
-stream.parameters.write(df)
+        for row in rows:
+            # For every currency we send value.
+            stream.parameters.buffer.add_timestamp(parser.parse(row['time'])) \
+                .add_value("{0}-{1}".format(from_currency, row['asset_id_quote']), row['rate']) \
+                .write()
 
-print("Closing stream")
+            print("{0}-{1}: {2}".format(from_currency, row['asset_id_quote'], row['rate']))
 
-# Stream can be infinitely long or have start and end.
-# If you send data into closed stream, it is automatically opened again.
-stream.close()
+        time.sleep(900)  # We sleep for 15 minute not to reach free account limit.
+    except Exception:
+        print(traceback.format_exc())
+
